bc3e7c83dcf7194385108ad48fd95e093cc1d68c
xdass <420600@gmail.com>
2017-11-11 12:53:26 +0500

    Add function to load url from file

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,23 @@
-def load_urls4check(path):
-    pass
+
+# whois server url - https://www.nic.ru/whois/?query=
+
+
+def load_urls4check(filepath):
+    try:
+        with open(filepath) as file:
+            sites_url = file.read()
+            return sites_url.split()
+    except FileNotFoundError:
+        return None
+
 
 def is_server_respond_with_200(url):
     pass
 
+
 def get_domain_expiration_date(domain_name):
     pass
 
+
 if __name__ == '__main__':
-    pass
+    print(load_urls4check('sites_url.txt'))
